47afd27ea7946ff7cac23566f8eef980deddcbf2
repiso-piyush55 <piyush.kumar@fiftytech.io>
2023-04-23 15:41:59 +0530

    working on making  nested-comment with navbar and post

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createTheme } from "@mui/material";
-import { ThemeProvider } from '@emotion/react';
+  import React from 'react';
+  import ReactDOM from 'react-dom/client';
+  import './index.css';
+  import App from './App';
+  import { createTheme } from "@mui/material";
+  import { ThemeProvider } from '@emotion/react';
 
-const theme = createTheme({
-  palette:{
-    buttonColor: '#3b8df3'
-  }
-});
+  const theme = createTheme({
+    palette:{
+      buttonColor: '#3b8df3'
+    }
+  });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider> 
-);
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider> 
+  );
