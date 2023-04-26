6724507bdd0d0cdde162df54c4408f0b6d8637c2
ShakshiAgrawal11 <shakshi.agrawal@fiftyfivetech.io>
2023-04-26 16:12:04 +0530

    new branch chatpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import LoginPage from '../src/pages/LoginPage/LoginPage';
-import CreateAccount from '../src/pages/SignupPage/CreateAccount';
-import ProfilePage from '../src/pages/ProfilePage/ProfilePage';
+
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import FeedLayout from './components/Layout/FeedLayout'
-import Bar from './components/SidebarNew/Bar';
-import ChatLayout from './components/Layout/ChatLayout'
-=======
+
 import '../src/css/style.css';
 import Navbar from  './components/Navbar';
 import LoginPage from './components/LoginPage';
@@ -15,7 +10,6 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ChatWindow from './components/chatWindow/ChatWindow';
 
 
->>>>>>> chatWindow
 
 function App() {
     return (     
