46dc065cfe3fa0fd7cda8901d909583182140424
Mohammed Ishaq <mohammed.ishaq@fiftyfivetech.io>
2023-04-28 15:59:39 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import Navbar from './components/navbar';
-import LoginPage from './components/LoginPage';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-// import Chat from './components/chat/Chat';
-
-=======
 import FinalLayout from './pages/FinalLayout/FinalLayout';
 import CreateAccount from '../src/pages/SignupPage/CreateAccount';
 import LoginPage from '../src/pages/LoginPage/LoginPage';
@@ -15,20 +8,10 @@ import FeedPage from '../src/pages/FeedPage/FeedPage';
 import ViewPost from './components/ViewPost/ViewPost';
 import FeedLayout from '../src/components/FeedLayout';
 import { Posts } from './components/post/Posts';
->>>>>>> login
 
 function App() {
 
     return (
-<<<<<<< HEAD
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route exact path="/userlogin" element={<LoginPage />} />
-            {/* <Route exact path="/chat" element={<Chat />} /> */}
-          </Routes>
-        </Router>
-=======
         <>
             <Router>
                 <Routes>
@@ -40,7 +23,6 @@ function App() {
                 </Routes>
             </Router>
         </>
->>>>>>> login
     );
 }
 
