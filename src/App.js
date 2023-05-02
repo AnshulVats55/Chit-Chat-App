01a999e127c91b9508b9b3ea011b3f778355f4b3
repiso-piyush55 <piyush.kumar@fiftytech.io>
2023-05-02 15:22:52 +0530

    comment component created and modified

--- src/App.js
+++ src/App.js
@@ -8,18 +8,23 @@ import FeedPage from '../src/pages/FeedPage/FeedPage';
 import ViewPost from './components/ViewPost/ViewPost';
 import FeedLayout from '../src/components/FeedLayout';
 import { Posts } from './components/post/Posts';
-
+import Comments from './components/simple-comments/Comments';
 function App() {
 
     return (
         <>
-            <Router>
+            {/* <Router>
                 <Routes>
                     <Route exact path="/" element={<LoginPage />} />
                     <Route exact path="/signup" element={<CreateAccount />} />
                     <Route exact path="/profile" element={<FinalLayout component={<ProfilePage />}/>} />
                     <Route exact path="/feedLayout" element={<FinalLayout component={<Posts />}/>} />
                     <Route exact path="/post" element={<FinalLayout component={<ViewPost />}/>} />
+                </Routes>
+            </Router> */}
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<Comments/>}></Route>
                 </Routes>
             </Router>
         </>
