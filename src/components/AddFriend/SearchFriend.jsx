f989eb5eb17f0ee24cee33f121fbb7f31aec42e4
Mohammed Ishaq <mohammed.ishaq@fiftyfivetech.io>
2023-05-18 12:35:12 +0530

    feature AddFriend

---
+++ src/components/AddFriend/SearchFriend.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { Paper, IconButton, Box, Avatar, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { ListStyles } from "../FriendList/FriendList.styles";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { useSelector } from "react-redux";
+import { socket } from "../../pages/CommonLayout/CommonLayout";
+import { allUSers } from "../../api/services/FriendRequestApi";
+
+const SearchFriend = ({ setshowAlertToast }) => {
+  const { classes } = ListStyles();
+  const [serchedUser, setSearchedUSer] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const userDetails = useSelector((state) => {
+    return state.userDataReducer[0];
+  });
+  const userId = userDetails?.data?.user.id;
+
+  const allFriends = useSelector((state) => {
+    return state.FriendsDataReducer;
+  });
+
+  const addFriend = (id) => {
+    socket.emit("addFriend", { followerUserId: userId, followedUserId: id });
+    setshowAlertToast({
+      visiblity: true,
+      message: "Friend Request Sent Succesfully !",
+      status: "success",
+    });
+  };
+
+  const searchHandler = async () => {
+    const users = await allUSers();
+    setSearchedUSer(users);
+  };
+
+  return (
+    <Box className={classes.searchFriendContStyles}>
+      <Typography variant="h6" className={classes.friendRequestText}>
+        Add New Friends
+      </Typography>
+      <Paper component="form" className={classes.searchContainer}>
+        <input
+          className={classes.searchBar}
+          placeholder="Start making friends..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyUp={searchHandler}
+        />
+        <IconButton type="button" className={classes.icon} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <Box className={classes.friendContainer}>
+        {serchedUser
+          ?.filter((val) => {
+            if (search === "") {
+              return null;
+            } else if (
+              (val.firstName.toLowerCase().startsWith(search.toLowerCase()) ||
+                val.lastName.toLowerCase().startsWith(search.toLowerCase())) &&
+              val.id !== userId
+            ) {
+              const index = allFriends[0]?.followers.findIndex((element) => {
+                return element.id === val.id;
+              });
+
+              if (index === -1) return val;
+              else return null;
+            } else {
+              return null;
+            }
+          })
+          .map((s) => {
+            const { id, firstName, lastName, profilePicture } = s;
+            return (
+              <Box className={classes.single} key={id}>
+                <Box className={classes.single1}>
+                  <Avatar
+                    className={classes.avatar}
+                    alt=""
+                    src={profilePicture}
+                  ></Avatar>
+                  <Typography sx={{ marginTop: "6px" }} variant="body1">
+                    {firstName + " " + lastName}
+                  </Typography>
+                  <IconButton onClick={() => addFriend(id)}>
+                    <PersonAddIcon />
+                  </IconButton>
+                </Box>
+              </Box>
+            );
+          })}
+      </Box>
+    </Box>
+  );
+};
+
+export default SearchFriend;
